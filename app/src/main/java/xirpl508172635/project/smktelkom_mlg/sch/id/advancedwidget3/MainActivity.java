41d5ad5075f3de1fcbf771ee6e99c3bcb6eb9f8d
Maulana Tito <titomaulana98@gmail.com>
2017-02-02 08:13:25 +0700

    Initial Commit

---
+++ app/src/main/java/xirpl508172635/project/smktelkom_mlg/sch/id/advancedwidget3/MainActivity.java
@@ -0,0 +1,13 @@
+package xirpl508172635.project.smktelkom_mlg.sch.id.advancedwidget3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
